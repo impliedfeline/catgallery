a86fb73ad28f5ecb3f28ce4f3ee04617180c5405
impliedfeline <kinnunen.jussi@hotmail.com>
2020-05-11 15:10:23 +0300

    Add model for pictures

---
+++ application/pictures/models.py
@@ -0,0 +1,8 @@
+from application import db
+
+class Picture(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+            onupdate=db.func.current_timestamp())
+
